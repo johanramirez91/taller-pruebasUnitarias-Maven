2db11231145f1baba7053d48851051ad19cc7347
Johan Ramirez <johan911019@gmail.com>
2021-12-17 15:30:26 -0500

    Se añaden los casos de prueba completos

--- src/test/java/co/com/sofka/app/BasicCalculatorTest.java
+++ src/test/java/co/com/sofka/app/BasicCalculatorTest.java
@@ -4,11 +4,15 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BasicCalculatorTest {
+
     private final BasicCalculator basicCalculator = new BasicCalculator();
+    private static final Logger logger = LoggerFactory.getLogger(BasicCalculatorTest.class);
 
     @Test
     @DisplayName("Testing sum: 1 + 1 = 2")
@@ -36,5 +40,56 @@ public class BasicCalculatorTest {
     void severalSums(Long first, Long second, Long expectedResult) {
         assertEquals(expectedResult, basicCalculator.sum(first, second),
                 () -> first + " + " + second + " should equal " + expectedResult);
+    }
+
+    @DisplayName("Testing several sums")
+    @ParameterizedTest(name = "{0} - {1} = {2}")
+    @CsvSource({
+            "5,    1,   4",
+            "1,    2,   -1",
+            "0,  1, -1",
+            "1,  1, 0",
+            "2, -4, 6",
+            "-5, -1, -4"
+    })
+    void severalSubs(Long first, Long second, Long expectedResult) {
+        assertEquals(expectedResult, basicCalculator.resta(first, second),
+                () -> first + " + " + second + " should equal " + expectedResult);
+    }
+
+    @DisplayName("Testing several sums")
+    @ParameterizedTest(name = "{0} * {1} = {2}")
+    @CsvSource({
+            "5,    1,   5",
+            "1,    2,   2",
+            "0,  1, 0",
+            "1,  1, 1",
+            "2, -4, -8",
+            "-10, -1, 10"
+    })
+    void severalMults(Long first, Long second, Long expectedResult) {
+        assertEquals(expectedResult, basicCalculator.multiplicacion(first, second),
+                () -> first + " + " + second + " should equal " + expectedResult);
+    }
+
+    @DisplayName("Testing several sums")
+    @ParameterizedTest(name = "{0} * {1} = {2}")
+    @CsvSource({
+            "5,    1,   5",
+            "1,    2,   0,5",
+            "0,  1, 0",
+            "1,  1, 1",
+            "2, -4, -0,5",
+            "-10, -1, 10",
+            "5, 0,",
+            "0, 0 "
+    })
+    void severalDivs(Long first, Long second, Long expectedResult) {
+        try {
+            assertEquals(expectedResult, basicCalculator.division(first, second),
+                    () -> first + " + " + second + " should equal " + expectedResult);
+        } catch (ArithmeticException error) {
+            logger.error("Error, division by zero" + error.getMessage());
+        }
     }
 }
