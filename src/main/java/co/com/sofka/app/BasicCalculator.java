6fc1909f1bc55c477445d74bdcbfa24e3678673f
Johan Ramirez <johan911019@gmail.com>
2021-12-17 15:01:13 -0500

    Se añaden los operadores resta, multiplicación y división

--- src/main/java/co/com/sofka/app/BasicCalculator.java
+++ src/main/java/co/com/sofka/app/BasicCalculator.java
@@ -6,8 +6,25 @@ import org.slf4j.LoggerFactory;
 public class BasicCalculator {
     private static final Logger logger = LoggerFactory.getLogger(BasicCalculator.class);
 
-    public Long sum(Long number1, Long number2){
+    public Long sum(Long number1, Long number2) {
         logger.info("Summing {} + {}", number1, number2);
         return number1 + number2;
+    }
+
+    public Long resta(Long number1, Long number2) {
+        logger.info("Substracting {} - {}", number1, number2);
+        return number1 - number2;
+    }
+
+    public Long multiplicacion(Long number1, Long number2) {
+        logger.info("Multiplicating {} * {}", number1, number2);
+        return number1 * number2;
+    }
+
+    public Long division(Long number1, Long number2) throws ArithmeticException {
+        if (number2 == 0)
+            throw new ArithmeticException("Number cannot be equals to 0, 'math indetermination'");
+        logger.info("Dividing {} / {}", number1, number2);
+        return number1 / number2;
     }
 }
